ebaf08fe3903879d675061ccffa5484ba7b420b8
MichaelM7X <mengzichen2010@gmail.com>
2025-02-24 04:24:07 -0500

    initial_commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function fetchNewDog() {
+    fetch("https://dog.ceo/api/breeds/image/random")
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById("random-dog").src = data.message;
+        })
+        .catch(error => console.error("Error fetching dog image:", error));
+}
+
+// Fetch a dog image on page load
+fetchNewDog();
